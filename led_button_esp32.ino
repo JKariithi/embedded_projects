2c3fcfcc4b48d12cb0cd9c6d237b35593dc53499
JKariithi <janicekariithi@gmail.com>
2025-07-01 12:10:54 +0300

    interfacing led with a push button.

---
+++ led_button_esp32.ino
@@ -0,0 +1,28 @@
+const int buttonPin = 15 ;     
+const int ledPin =  23;    
+
+int buttonState = 0;        // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == LOW) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    delay(5000);
+    digitalWrite(ledPin, LOW);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    
+  }
+}
